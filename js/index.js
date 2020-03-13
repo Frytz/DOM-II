9641c6251c9a9da6496c438a2911f6a71b629e63
John Rossi <DACOOK1983@gamil.com>
2020-03-13 18:44:13 -0400

    6 uniques completed

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
-//mouseover
+//MVP challenges
 
-// let test = document.getElementsByTagName('nav');
-let mEnter = document.querySelector("nav");
-console.log(mEnter);
-// mouseenter not working
+//mouseenter (1)
+
+
+const mEnter = document.querySelector("nav");
+// console.log(mEnter);
+
 mEnter.addEventListener('mouseenter', function( event ){
     event.target.style.border = "5px dotted purple";
 
+
     setTimeout(function()
     {
-        event.target.style.border = "";
-    }, 500);
+        event.target.style.border = "5px dotted red";
+    }, 100);
+
 },false);
 
-let test = document.getElementById("navId");
+//mouseExit (2)
+
+const mExit = document.querySelector("nav");
+
+mExit.addEventListener('mouseleave', e => {
+    mExit.style.border = "";
+},false);
+
+//mouseover (3)
+const test = document.getElementById("navId");
 console.log(test);
 test.addEventListener("mouseover", function( event ) {   
     // highlight the mouseover target
@@ -24,6 +37,43 @@ test.addEventListener("mouseover", function( event ) {
       event.target.style.color = "";
     }, 500);
   }, false);
+
+  //click(4)
+const mClick = document.getElementById("navId");
+
+const onclick = event => event.target.style.transform = "scale(1.2)"
+
+mClick.addEventListener("click", (event) => {
+    event.target.style.transform = "scale(1.2)";
+    
+    setTimeout (function(){
+        event.target.style.transform = "scale(1)";
+    },500);
+},false);
+  
+//doubleclick(5)
+
+const mDblClick = document.getElementById("navId");
+
+mDblClick.addEventListener("dblclick", (event) => {
+    window.alert("Nav Does Not Work");
+
+},false);
+
+//load(6)
+window.addEventListener('load', (event) => {
+    window.alert('page has loaded');
+});
+
+//dragable
+
+dragElement (document.getElementById("modalOne"));
+
+function dragElement(elemnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 4;
+    if (elemnt.onmousedown)
+}
+
 
   //form popup
   var modalBtns = [...document.querySelectorAll(".child-btn")];
